c7cb5c381e7ad174311424379c476dfb55608472
Mau <natanielmauricioarapaestrada@gmail.com>
2025-05-21 21:28:22 -0400

    arreglando

--- bd/bd_comp.sql
+++ bd/bd_comp.sql
@@ -2,8 +2,8 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Servidor: 127.0.0.1
--- Tiempo de generación: 21-05-2025 a las 23:32:04
+-- Servidor: localhost:3307
+-- Tiempo de generación: 22-05-2025 a las 03:27:59
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -32,7 +32,10 @@ CREATE TABLE `mensajes` (
   `id_usuarios` int(11) NOT NULL,
   `asunto` varchar(50) NOT NULL,
   `descripcion` varchar(100) NOT NULL,
-  `estado` varchar(50) NOT NULL
+  `estado` varchar(50) NOT NULL,
+  `bandeja` varchar(20) NOT NULL,
+  `id_remitente` int(11) NOT NULL,
+  `id_destinatario` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -46,16 +49,17 @@ CREATE TABLE `usuario` (
   `correo` varchar(100) NOT NULL,
   `password` varchar(100) NOT NULL,
   `nombre` varchar(40) NOT NULL,
-  `nivel` int(1) NOT NULL
+  `nivel` int(1) NOT NULL,
+  `estado` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `usuario`
 --
 
-INSERT INTO `usuario` (`id`, `correo`, `password`, `nombre`, `nivel`) VALUES
-(1, 'clase@gmail.com', '8cb2237d0679ca88db6464eac60da96345513964', 'laboratorio', 0),
-(2, 'jose@gmail.com', 'sis', 'jose', 1);
+INSERT INTO `usuario` (`id`, `correo`, `password`, `nombre`, `nivel`, `estado`) VALUES
+(1, 'clase@gmail.com', '8cb2237d0679ca88db6464eac60da96345513964', 'laboratorio', 0, 0),
+(2, 'jose@gmail.com', 'sis', 'jose', 1, 0);
 
 --
 -- Índices para tablas volcadas
---
+++ script.js
@@ -0,0 +1,1 @@
+function redirigir_pagina(){};
