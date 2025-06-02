28df154788181386b9d3489ff9b4c7b15ccb5c47
MjlMedrano <adrian.mijael.medrano@gmail.com>
2025-06-02 17:26:39 -0400

    Tarea completada

--- LAB5/conexion.php
+++ LAB5/conexion.php
@@ -1,5 +1,5 @@
 <?php 
-$con=mysqli_connect("localhost","root","","bd_comp");
+$con=mysqli_connect("localhost","root","","bd_hotel");
 if(mysqli_connect_errno()){
     die("Se produjo un error ".mysqli_connect_error());
 }
