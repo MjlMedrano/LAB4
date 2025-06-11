2cd3560b5f85d4903aa5ed39060f7e0d1731ba18
Mau <natanielmauricioarapaestrada@gmail.com>
2025-06-11 16:05:07 -0400

    pinchi puerto

--- NewLab5/conexion.php
+++ NewLab5/conexion.php
@@ -1,5 +1,5 @@
 <?php 
-$con=mysqli_connect("localhost","root","","bd_hotel",33065);
+$con=mysqli_connect("localhost","root","","bd_hotel");
 if(mysqli_connect_errno()){
     die("Se produjo un error ".mysqli_connect_error());
 }
